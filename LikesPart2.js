e21b539228de5cd6da025fe64b9648fff2007f84
Venus Quan <94498212+vxq5000@users.noreply.github.com>
2022-03-01 14:39:55 -0800

    Add files via upload

---
+++ LikesPart2.js
@@ -0,0 +1,23 @@
+var count=9;
+var countElement= document.querySelector(".numLike");
+function add1(){
+    count ++;
+    countElement.innerText = count+ " like(s)"
+    console.log(count);
+}
+
+var count2=12;
+var countElement2= document.querySelector(".numLike-2");
+function add2(){
+    count2 ++;
+    countElement2.innerText = count2+ " like(s)"
+    console.log(count);
+}
+
+var count3=9;
+var countElement3= document.querySelector(".numLike-3");
+function add3(){
+    count3 ++;
+    countElement3.innerText = count3+ " like(s)"
+    console.log(count);
+}
